63e959ad451abde6c9a4652ecff350c705548a0b
“cosykid” <langkee.hong@gmail.com>
2025-01-16 01:34:48 +1300

    bug fixed and ready for digitalocean

--- src/duel.py
+++ src/duel.py
@@ -11,6 +11,18 @@ GUN_GIFS = [
     'https://tenor.com/view/bocchi-the-rock-hitori-gotoh-bocchi-yamcha-birds-gif-27233195'
 ]
 
+DUEL_MESSAGES = [
+    '{winner.mention} hit {loser.mention} with a a 6-string bass',
+    '{loser.mention} was sent the squid games by {winner.mention}',
+    '{loser.mention} was sent to the summoner\'s rift by {winner.mention}',
+    '{winner.mention} threw {loser.mention} into orbit',
+    '{loser.mention} was suplexed by {winner.mention}',
+    '{loser.mention} got mogged by {winner.mention}',
+    '{winner.mention} was too alpha for {loser.mention}',
+    '{winner.mention} installed league of legends on {loser.mention}\'s computer',
+    '{winner.mention} emoted on {loser.mention}'
+]
+
 def register_duel_command(tree, client):
     @tree.command(
         name="duel",
@@ -30,13 +42,21 @@ def register_duel_command(tree, client):
             return
         
         # Randomly determine the outcome of the duel
-        loser = random.choice([interaction.user, member])
-        winner = interaction.user if loser == member else member
+        loser = None
+        winner = None
+
+        if (random.choice([interaction.user, member]) == member):
+            winner = member
+            loser = await interaction.guild.fetch_member(interaction.user.id)
+        else:
+            winner = interaction.user
+            loser = member
 
         try:
             # Attempt to timeout the member
-            await member.timeout(timedelta(minutes=1), reason="You lost a duel!")
-            await interaction.response.send_message(f'{winner.mention} beat {loser.mention} in the duel!... I\'m gonna head home')
+            await loser.timeout(timedelta(minutes=2), reason="You lost a duel!")
+            message = random.choice(DUEL_MESSAGES).format(winner=winner, loser=loser)
+            await interaction.response.send_message(message)
             await interaction.followup.send(random.choice(GUN_GIFS))
         except discord.Forbidden:
-            await interaction.response.send_message(f"{member.mention} lost the duel, but can't be timed out... sorry")
+            await interaction.response.send_message(f"{loser.mention} lost the duel, but can't be timed out... sorry")
